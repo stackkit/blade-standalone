0350e07177152f7c4bbd26c204aa00e6ab867743
Marick van Tuil <info@marickvantuil.nl>
2017-12-19 19:33:00 +0100

    Initial commit

---
+++ src/Blade.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Stackkit\BladeStandalone;
+
+use Illuminate\Events\Dispatcher;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\View\Compilers\BladeCompiler;
+use Illuminate\View\Engines\CompilerEngine;
+use Illuminate\View\Engines\EngineResolver;
+use Illuminate\View\Factory;
+use Illuminate\View\FileViewFinder;
+
+class Blade
+{
+    private $factory;
+
+    function __construct($viewsDir, $cacheDir)
+    {
+        $resolver = new EngineResolver;
+
+        $resolver->register('blade', function () use ($viewsDir, $cacheDir) {
+            return new CompilerEngine(new BladeCompiler(new Filesystem(), $cacheDir));
+        });
+
+        $this->factory = new Factory($resolver, new FileViewFinder(new Filesystem(), [$viewsDir]), new Dispatcher());
+    }
+
+    function render($view, $variables)
+    {
+        return $this->factory->make($view, $variables)->render();
+    }
+}
